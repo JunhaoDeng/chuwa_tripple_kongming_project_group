91a64ccd772caf099e1bad0c3946c8a362623f82
Chenjie Wu <wucj.eric2015@outlook.com>
2024-06-18 23:37:16 -0700

    written some comments

--- chuwa_proj1_backend/database/db.ts
+++ chuwa_proj1_backend/database/db.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+
+// write your own pwd and uri 
 const pwd: string = process.env.MONGODB_ATLAS_PWD || "";
 const uri: string = `mongodb+srv://degjnd:${encodeURIComponent(pwd)}@cluster0.hpelqzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Call this function to connect to the mongodb atlas database
 export function connect_db() {
     mongoose.connect(uri).then(() => {
         console.log("Connected");
--- chuwa_proj1_backend/database/query.ts
+++ chuwa_proj1_backend/database/query.ts
@@ -3,6 +3,8 @@ import { Account, Product, Cart } from "./schema";
 import { AccountDataType, CartDataType, IDString, ProductDataType } from "./type"
 import { insert_nonnull_prop } from "./utility"
 
+// create an account
+// RETURN: a Promise either resolve to the new account id, or rejected.   
 // this method only add an account. 
 // It does not automatically create a cart for the account
 export function create_account(email: string, enc_password: string, type: string, 
@@ -16,17 +18,19 @@ export function create_account(email: string, enc_password: string, type: string
         enc_password: enc_password,
         type: type
     }
-    if (cart !== null) {
-        data.cart = cart;
-    }
+    insert_nonnull_prop(data, "cart", cart);
 
     return Account.create(data).then(acc => { return acc._id; });
 }
 
+// find an account based on email
+// RETURN: a promise resolve to document|null, or rejected. 
 export function get_account_by_email(email: string): Promise<any> {
     return Account.findOne({email: email});
 }
 
+// create a cart belongs to an account
+// RETURN: a promise resolve to new cart's id, or rejected. 
 export function create_cart(zip_code: string | null, discount_cent: number, 
             estimated_total_cent: number, belong_to: string): Promise<IDString> {
 
@@ -34,9 +38,7 @@ export function create_cart(zip_code: string | null, discount_cent: number,
         discount_cent: discount_cent,
         estimated_total_cent: estimated_total_cent,
     };
-    if (zip_code !== null) {
-        cart.zip_code = zip_code;
-    }
+    insert_nonnull_prop(cart, "zip_code", zip_code);
     let cartid: string;
     return Cart.create(cart).then(crt => {
         cartid = crt._id;
@@ -48,10 +50,14 @@ export function create_cart(zip_code: string | null, discount_cent: number,
     })
 }
 
-export function update_password_by_email(email: string, pwd: string) {
+// update password of an account based on its email
+// RETURN: a promise resolve to update result. 
+export function update_password_by_email(email: string, pwd: string) : Promise<any> {
     return Account.findOneAndUpdate({ email: email }, { enc_password: pwd }, { new: true });
 }
 
+// create a product belongs to an account
+// RETURN: a promise resolve to product id, or rejected. 
 export function create_product(name: string, description: string | null, 
         category: string | null, price_cent: number, quantity: number,
         img_link: string | null, created_by: string) : Promise<IDString> {
@@ -64,9 +70,6 @@ export function create_product(name: string, description: string | null,
         create_time: d,
         update_time: d
     };
-    // if (description !== null) {
-    //     product.description
-    // }
     insert_nonnull_prop(product, "description", description);
     insert_nonnull_prop(product, "category", category);
     insert_nonnull_prop(product, "img_link", img_link);
