fad47764ef2bb9069ff46cee63c4a71160ccf797
JunhaoDeng <48834693+JunhaoDeng@users.noreply.github.com>
2024-07-09 01:18:10 -0700

    no change

--- chuwa_proj1_frontend/src/components/SigninForm.tsx
+++ chuwa_proj1_frontend/src/components/SigninForm.tsx
@@ -1,89 +1,122 @@
-import '../styles/Form.css';
-import Field from './Field';
-import { CloseOutlined } from '@ant-design/icons';
-import { RootState } from '../redux/store';
-import { signinSetEmail, signinSetEmailErrormsg, signinSetPassword, signinSetPasswordErrormsg } from '../redux/slice';
-import { HOST } from '../config';
-import { useSelector } from 'react-redux';
+import "../styles/Form.css";
+import Field from "./Field";
+import { CloseOutlined } from "@ant-design/icons";
+import { RootState } from "../redux/store";
+import {
+  signinSetEmail,
+  signinSetEmailErrormsg,
+  signinSetPassword,
+  signinSetPasswordErrormsg,
+} from "../redux/slice";
+import { HOST } from "../config";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 type SigninDataType = {
-  email: string,
-  enc_password: string
+  email: string;
+  enc_password: string;
 };
 
 // a's will be replaced by Link element in the future
 
 export default function SigninForm() {
-    const checkEmailFunc = (data: string) => {
-        // eslint-disable-next-line no-useless-escape
-        if (data.length === 0) {
-            return "Email cannot be empty";
-        }
-        return "";
-      }
-  
-    const checkPasswordFunc = (data: string) => {
-        if (data.length === 0) {
-            return "Password cannot be empty";
-        }
-        return "";
-      }
-    
-    const emailSelector = (state: RootState) => state.signin.email;
-    
-    const passwordSelector = (state: RootState) => state.signin.password;
-  
-    const email: string = useSelector(emailSelector);
-    const password: string = useSelector(passwordSelector);
+  const navigate = useNavigate();
+  const checkEmailFunc = (data: string) => {
+    // eslint-disable-next-line no-useless-escape
+    if (data.length === 0) {
+      return "Email cannot be empty";
+    }
+    return "";
+  };
 
-    const handleFormSubmit = () => {
-      const signinData: SigninDataType = {
-        email: email,
-        enc_password: password
-      };
-      console.log(signinData);
-      
-      const options = {
-        method: "POST",
-        body: JSON.stringify(signinData),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }
-      fetch(`${HOST}/api/auth/signin`, options)
-      .then(response => response.json())
-      .then(data => {
+  const checkPasswordFunc = (data: string) => {
+    if (data.length === 0) {
+      return "Password cannot be empty";
+    }
+    return "";
+  };
+
+  const emailSelector = (state: RootState) => state.signin.email;
+
+  const passwordSelector = (state: RootState) => state.signin.password;
+
+  const email: string = useSelector(emailSelector);
+  const password: string = useSelector(passwordSelector);
+
+  const handleFormSubmit = () => {
+    const signinData: SigninDataType = {
+      email: email,
+      enc_password: password,
+    };
+    console.log(signinData);
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(signinData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    fetch(`${HOST}/api/auth/signin`, options)
+      .then((response) => response.json())
+      .then((data) => {
         console.log("success");
         console.log(data.token);
-        sessionStorage.setItem("token", data.token)
+        sessionStorage.setItem("token", data.token);
       })
-      .catch(err => {
+      .catch((err) => {
         alert("Authentication failed: " + err);
-      })
-    }
-    return (
-        <div className='form_container'>
-          <div className='form'>
-            <div className='close_icon_container'><CloseOutlined /></div>
-            <div className='form_title'>Sign in to your account</div>
-            <Field label='Email' ssid='signin_email' type='email' checkFunc={ checkEmailFunc } 
-                placeholder='Enter your email' 
-                inputDataSelectorFunc={ emailSelector }
-                errormsgDataSelectorFunc={ (state: RootState) => state.signin.email_errormsg }
-                inputDataAction={ signinSetEmail }
-                errormsgAction={ signinSetEmailErrormsg }/>
-            <Field label='Password' ssid='signin_password' type='password' checkFunc={ checkPasswordFunc }
-                placeholder='Enter your password' 
-                inputDataSelectorFunc={ passwordSelector }
-                errormsgDataSelectorFunc={ (state: RootState) => state.signin.password_errormsg}
-                inputDataAction={ signinSetPassword }
-                errormsgAction={ signinSetPasswordErrormsg }/>
-            <button className='form_submit_button' onClick={ handleFormSubmit }>Sign in</button>
-            <div className='bottomrow'> 
-                <div className='alt'>Don't have an account? <a className='link' href='google.com'>Sign up</a></div>
-                <a className='link' href='google.com'>Forgot password?</a>
-            </div>
-          </div>
+      });
+    navigate("/products");
+  };
+  return (
+    <div className="form_container">
+      <div className="form">
+        <div className="close_icon_container">
+          <CloseOutlined />
         </div>
-    )
+        <div className="form_title">Sign in to your account</div>
+        <Field
+          label="Email"
+          ssid="signin_email"
+          type="email"
+          checkFunc={checkEmailFunc}
+          placeholder="Enter your email"
+          inputDataSelectorFunc={emailSelector}
+          errormsgDataSelectorFunc={(state: RootState) =>
+            state.signin.email_errormsg
+          }
+          inputDataAction={signinSetEmail}
+          errormsgAction={signinSetEmailErrormsg}
+        />
+        <Field
+          label="Password"
+          ssid="signin_password"
+          type="password"
+          checkFunc={checkPasswordFunc}
+          placeholder="Enter your password"
+          inputDataSelectorFunc={passwordSelector}
+          errormsgDataSelectorFunc={(state: RootState) =>
+            state.signin.password_errormsg
+          }
+          inputDataAction={signinSetPassword}
+          errormsgAction={signinSetPasswordErrormsg}
+        />
+        <button className="form_submit_button" onClick={handleFormSubmit}>
+          Sign in
+        </button>
+        <div className="bottomrow">
+          <div className="alt">
+            Don't have an account?{" "}
+            <a className="link" href="google.com">
+              Sign up
+            </a>
+          </div>
+          <a className="link" href="google.com">
+            Forgot password?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- chuwa_proj1_frontend/src/pages/ProductDetailPage/index.tsx
+++ chuwa_proj1_frontend/src/pages/ProductDetailPage/index.tsx
@@ -1,110 +1,134 @@
-import React, { useEffect } from 'react';
-import styles from '../../styles/Products.module.css';
-import btnStyles from '../../styles/Btn.module.css';
-import responsiveStyles from '../../styles/Adjustor.module.css';
-import { useParams } from "react-router-dom"
+import React, { useEffect } from "react";
+import styles from "../../styles/Products.module.css";
+import btnStyles from "../../styles/Btn.module.css";
+import responsiveStyles from "../../styles/Adjustor.module.css";
+import { useParams } from "react-router-dom";
 // const style: React.CSSProperties = { background: '#0092ff', padding: '8px 0' };
-import { Button, Card, Flex, Typography } from 'antd';
-import { HOST } from '../../config';
-import { jwtDecode } from 'jwt-decode';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
+import { Button, Card, Flex, Typography } from "antd";
+import { HOST } from "../../config";
+import { jwtDecode } from "jwt-decode";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
 const cardStyle: React.CSSProperties = {
-    width: "100%",
-    height: "100%",
-    padding: "2rem",
-    border: '0',
-    borderRadius: "0px",
-    alignItems: 'center'
+  width: "100%",
+  height: "100%",
+  padding: "2rem",
+  border: "0",
+  borderRadius: "0px",
+  alignItems: "center",
 };
 
 const imgStyle: React.CSSProperties = {
-    display: 'block',
-    width: "50%",
-    borderRadius: '0',
-    alignSelf: 'center'
+  display: "block",
+  width: "50%",
+  borderRadius: "0",
+  alignSelf: "center",
 };
-
 
 const { Title, Paragraph, Text } = Typography;
 
-
-
 const ProductDetailPage: React.FC = () => {
-    // const { product_id } = useParams();
-    const params_product_id = "668b578c4cbb11b551187d55";
+  // const { product_id } = useParams();
+  const params_product_id = "668b578c4cbb11b551187d55";
 
-    const product_id: string = useSelector((state: RootState) => state.product_detail.id);
-    const name: string = useSelector((state: RootState) => state.product_detail.name);
-    const price: number = useSelector((state: RootState) => state.product_detail.price);
-    const num_added: number = useSelector((state: RootState) => state.product_detail.num_added);
-    const image_link: string = useSelector((state: RootState) => state.product_detail.image_link);
-    const description: string = useSelector((state: RootState) => state.product_detail.description);
-    const category: string = useSelector((state: RootState) => state.product_detail.category);
+  const product_id: string = useSelector(
+    (state: RootState) => state.product_detail.id
+  );
+  const name: string = useSelector(
+    (state: RootState) => state.product_detail.name
+  );
+  const price: number = useSelector(
+    (state: RootState) => state.product_detail.price
+  );
+  const num_added: number = useSelector(
+    (state: RootState) => state.product_detail.num_added
+  );
+  const image_link: string = useSelector(
+    (state: RootState) => state.product_detail.image_link
+  );
+  const description: string = useSelector(
+    (state: RootState) => state.product_detail.description
+  );
+  const category: string = useSelector(
+    (state: RootState) => state.product_detail.category
+  );
 
-    useEffect(() => {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        let decoded: any = null;
-        try {
-            decoded = jwtDecode(sessionStorage.getItem("token") as string);
-        } catch(err) {
-            alert("Token invalid or missing");
-            return;
-        }
-        console.log(decoded);
-        
-        const options = {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${sessionStorage.getItem("token")}`
-            }
-        };
-        fetch(`${HOST}/api/users/${decoded.id}/product/${params_product_id}`, options)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            return fetch(`${HOST}/api/users/${decoded.id}/cart`);
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            
-        })
-    }, []);
-    return (
-        <div className={styles.pageWrapper}>
-            <div className={styles.titleWrapper}>
-                <h1 className={styles.title}>Products Detail</h1>
-            </div>
-            {/* <div className={styles.productsBoard}> */}
-            <Card style={cardStyle} styles={{ body: { padding: 0, overflow: 'hidden', height: '100%' } }}>
-                <Flex className={responsiveStyles.responsiveWrap} justify="space-between" style={{ height: '100%' }} gap='large'>
-                    <img
-                        alt="avatar"
-                        src={ image_link }
-                        style={imgStyle}
-                    />
-                    <Flex vertical align="flex-start" >
-                        <Typography>
-                            <Text>{ category }</Text>
-                            <Title style={{margin: 0}} level={2}>{ name }</Title>
-                            <Title style={{margin: '1rem 0'}} level={2}>${(price / 100).toFixed(2)}</Title>
-                            <Paragraph>
-                                { description }
-                            </Paragraph>
-                        </Typography>
-                        <Flex style={{width: '70%', marginTop: '1rem'}} gap="small">
-                            <Button className={btnStyles.uniformPrimaryBtn} style={{ flex: '1 1 0' }} type="primary">Add To Cart</Button>
-                            <Button style={{ flex: '1 1 0' }}>Edit</Button>
-                        </Flex>
-                    </Flex>
-                </Flex>
-            </Card>
-            {/* </div> */}
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let decoded: any = null;
+    try {
+      decoded = jwtDecode(sessionStorage.getItem("token") as string);
+    } catch (err) {
+      alert("Token invalid or missing");
+      return;
+    }
+    console.log(decoded);
 
-        </div>
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+    };
+    fetch(
+      `${HOST}/api/users/${decoded.id}/product/${params_product_id}`,
+      options
     )
-}
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return fetch(`${HOST}/api/users/${decoded.id}/cart`);
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.titleWrapper}>
+        <h1 className={styles.title}>Products Detail</h1>
+      </div>
+      {/* <div className={styles.productsBoard}> */}
+      <Card
+        style={cardStyle}
+        styles={{ body: { padding: 0, overflow: "hidden", height: "100%" } }}
+      >
+        <Flex
+          className={responsiveStyles.responsiveWrap}
+          justify="space-between"
+          style={{ height: "100%" }}
+          gap="large"
+        >
+          <img alt="avatar" src={image_link} style={imgStyle} />
+          <Flex vertical align="flex-start">
+            <Typography>
+              <Text>{category}</Text>
+              <Title style={{ margin: 0 }} level={2}>
+                {name}
+              </Title>
+              <Title style={{ margin: "1rem 0" }} level={2}>
+                ${(price / 100).toFixed(2)}
+              </Title>
+              <Paragraph>{description}</Paragraph>
+            </Typography>
+            <Flex style={{ width: "70%", marginTop: "1rem" }} gap="small">
+              <Button
+                className={btnStyles.uniformPrimaryBtn}
+                style={{ flex: "1 1 0" }}
+                type="primary"
+              >
+                Add To Cart
+              </Button>
+              <Button style={{ flex: "1 1 0" }}>Edit</Button>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Card>
+      {/* </div> */}
+    </div>
+  );
+};
 
 export default ProductDetailPage;
