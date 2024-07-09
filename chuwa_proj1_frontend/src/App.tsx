5a840eb2949e922b0020dc606eae6e0c8e504df3
JunhaoDeng <48834693+JunhaoDeng@users.noreply.github.com>
2024-07-08 19:56:13 -0700

    add routes and errorpage

--- chuwa_proj1_frontend/src/App.tsx
+++ chuwa_proj1_frontend/src/App.tsx
@@ -1,26 +1,48 @@
-import Layout from './components/layout/index';
-import ProductsPage from './pages/ProductsPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import store from './redux/store';
-import { Provider } from 'react-redux';
-import SignUpForm from './components/SignupForm';
-import CreateProductForm from './components/CreateProductForm';
+import Layout from "./components/layout/index";
+import ProductsPage from "./pages/ProductsPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import SignUpForm from "./components/SignupForm";
+import SigninForm from "./components/SigninForm";
+import UpdatePwdForm from "./components/UpdatePwdForm";
+import CreateProductForm from "./components/CreateProductForm";
+import ErrorPage from "./components/ErrorPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import './App.css'
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Provider store={store}>
         <Layout>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/signin" element={<SigninForm />} />
+              <Route path="/signup" element={<SignUpForm />} />
+              <Route path="/update-password" element={<UpdatePwdForm />} />
+              <Route path="/update-success" element={<SigninForm />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/products/create" element={<CreateProductForm />} />
+              <Route
+                path="/products/:productId/edit"
+                element={<CreateProductForm />}
+              />
+              <Route
+                path="/products/:productId/detail"
+                element={<ProductDetailPage />}
+              />
+              <Route path="/error" element={<ErrorPage />} />
+            </Routes>
+          </BrowserRouter>
           {/* <ProductsPage /> */}
           {/* <ProductDetailPage /> */}
           {/* <SignUpForm /> */}
-          <CreateProductForm />
+          {/* <CreateProductForm /> */}
         </Layout>
       </Provider>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
